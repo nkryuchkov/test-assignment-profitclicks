8b8bfafe62e125fcd9d1cc860409c15f3de66c55
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2018-12-17 21:13:31 +0300

    Implement API router

---
+++ api/api.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/nkryuchkov/test-assignment-profitclicks/logger"
+)
+
+const (
+	v1Prefix = "/api/v1"
+)
+
+// Config represents an API configuration.
+type Config struct {
+	Port int `json:"port"`
+}
+
+// API represents a REST API server instance.
+type API struct {
+	config *Config
+	log    *logger.Logger
+}
+
+// New returns a new API instance.
+func New(config *Config, log *logger.Logger) *API {
+	api := &API{
+		config: config,
+		log:    log,
+	}
+
+	return api
+}
+
+// Start starts the API server.
+func (api *API) Start() error {
+	api.log.Infof("Starting API")
+
+	r := mux.NewRouter()
+	s := r.PathPrefix(v1Prefix).Subrouter()
+
+	s.HandleFunc("/number", api.addNumberToList).Methods("POST")
+	s.HandleFunc("/list", api.addNumberList).Methods("POST")
+	s.HandleFunc("/list", api.deleteNumberList).Methods("DELETE")
+	s.HandleFunc("/operation", api.addOperationToList).Methods("POST")
+	s.HandleFunc("/operation", api.addNumberList).Methods("GET")
+
+	return http.ListenAndServe(":"+strconv.Itoa(api.config.Port), r)
+}
---
+++ api/handlers.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"net/http"
+)
+
+func (api *API) addNumberToList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (api *API) addNumberList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (api *API) deleteNumberList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (api *API) addOperationToList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (api *API) getListResult(w http.ResponseWriter, r *http.Request) {
+
+}
