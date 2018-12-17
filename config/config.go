19095b1489a50eeab26d2d01887fb72bc7f2d929
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2018-12-17 21:26:34 +0300

    Implement config parsing

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/nkryuchkov/test-assignment-profitclicks/api"
+	"github.com/nkryuchkov/test-assignment-profitclicks/logger"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	filename = "config.json"
+)
+
+// Config represents an application configuration.
+type Config struct {
+	Logger *logger.Config `json:"logger"`
+	API    *api.Config    `json:"api"`
+}
+
+// FromFile reads a config from the file specified in `filename`.
+func FromFile() (*Config, error) {
+	configFilename := filename
+	if len(os.Args) > 1 {
+		configFilename = os.Args[1]
+	}
+
+	configFilePath, err := filepath.Abs(configFilename)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not find config absolute path")
+	}
+
+	log.Printf("Loading config: %v", configFilePath)
+
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not open config file")
+	}
+
+	defer func() {
+		if err = configFile.Close(); err != nil {
+			log.Printf("Could not close config file: %v", err)
+		}
+	}()
+
+	var config Config
+	if err = json.NewDecoder(configFile).Decode(&config); err != nil {
+		return nil, errors.Wrapf(err, "could not read config file")
+	}
+
+	return &config, nil
+}
