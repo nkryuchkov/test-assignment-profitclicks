e2f4c65297e900f5e1b0d94a003b2eca6b526379
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2018-12-18 00:23:59 +0300

    Implement a minor query fix

--- storage/repository.go
+++ storage/repository.go
@@ -2,12 +2,12 @@ package storage
 
 // AddNumberList inserts a new number list with given uid in the lists table.
 func (s *Storage) AddNumberList(uid string) error {
-	_, err := s.getConnection().Exec(`INSERT INTO lists (uid) VALUES(?)`, uid)
+	_, err := s.getConnection().Exec("INSERT INTO `lists` (uid) VALUES (?)", uid)
 	return err
 }
 
 // DeleteNumberList deletes a number list with given uid from the lists table.
 func (s *Storage) DeleteNumberList(uid string) error {
-	_, err := s.getConnection().Exec("DELETE FROM lists WHERE uid = (?)", uid)
+	_, err := s.getConnection().Exec("DELETE FROM `lists` WHERE uid = (?)", uid)
 	return err
 }
